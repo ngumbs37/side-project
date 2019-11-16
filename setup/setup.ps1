4909fb83a132fd0736e68a6d2fc45211e68ae745
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-11-15 23:39:13 -0600

    remove script

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -4,10 +4,10 @@ $outpath = ".\node-v12.13.0-x64.msi"
 if (!(Test-Path $outpath)){
   Invoke-WebRequest -Uri $url -OutFile $outpath
 }
-Start-Process -Filepath $outpath
 $url = "https://s3.amazonaws.com/aws-cli/AWSCLI64PY3.msi"
 $outpath2 = ".\AWSCLISetup.msi"
 if (!(Test-Path $outpath2)){
   Invoke-WebRequest -Uri $url -OutFile $outpath2
 }
-Start-Process -Filepath $outpath2
+Start-Process -Filepath $outpath -Wait
+Start-Process -Filepath $outpath2 -Wait
