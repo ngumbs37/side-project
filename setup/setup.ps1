75c1f6424e9389f5ed4d1b33619bd1bf02672bb0
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-11-15 23:24:02 -0600

    rework setup script

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -1,9 +1,13 @@
 Set-ExecutionPolicy -ExecutionPolicy Undefined  -Scope CurrentUser
 $url = "https://nodejs.org/dist/v12.13.0/node-v12.13.0-x64.msi"
 $outpath = ".\node-v12.13.0-x64.msi"
-Invoke-WebRequest -Uri $url -OutFile $outpath
+if (!(Test-Path $outpath)){
+  Invoke-WebRequest -Uri $url -OutFile $outpath
+}
 Start-Process -Filepath $outpath
-$url = "https://s3.amazonaws.com/aws-cli/AWSCLISetup.exe"
-$outpath2 = ".\AWSCLISetup.exe"
-Invoke-WebRequest -Uri $url -OutFile $outpath2
-Start-Process -Filepath outpath2
+$url = "https://s3.amazonaws.com/aws-cli/AWSCLI64PY3.msi"
+$outpath2 = ".\AWSCLISetup.msi"
+if (!(Test-Path $outpath2)){
+  Invoke-WebRequest -Uri $url -OutFile $outpath2
+}
+Start-Process -Filepath $outpath2
