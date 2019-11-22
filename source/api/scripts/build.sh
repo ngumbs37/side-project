903c8d3b7a29b6bdae251d59a8af1a27d524e679
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-11-22 02:36:40 -0600

    add example template and update build script

--- source/api/scripts/build.sh
+++ source/api/scripts/build.sh
@@ -1,12 +1,13 @@
-s3_bucket=sally-chata
-for i in $(find . -name '*.yml'); do
+s3_bucket=$S3_BUCKET
+for i in $(find ./src -name '*.yml' -not -path "**/.aws-sam/*" ); do
     filename=$(basename $i)
     directory=$(dirname $i)
-    yml_to_yaml=${filename/yml/yaml}
+    local_aws_sam=$directory"/.aws-sam/" # local built artifacts of the lambda
+    yml_to_yaml=${filename/yml/yaml} # 
     path_for_compiled_templates='templates/'$yml_to_yaml
-    echo $path_for_compiled_templates
-    sam build -s $directory -t $i
-    sam package --template-file $i --s3-bucket $s3_bucket --output-template-file $path_for_compiled_templates
+    rm -rf $local_aws_sam # remove local built package for deployment
+    sam build -s $directory -t $i # builds package to deploy
+    sam package --template-file $i --s3-bucket $s3_bucket --output-template-file $path_for_compiled_templates # uploads pagkage and uploads to s3
     #aws cloudformation package --template-file $i --s3-bucket $s3_bucket --output-template-file $path_for_compiled_templates
 done
 aws s3 cp templates s3://$s3_bucket --recursive
