2972206a43e79a831b07b2ed7f8e170d3f9ffba3
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-11-29 00:03:47 -0600

    remove local variable, exclude other .yml files

--- source/api/scripts/build.sh
+++ source/api/scripts/build.sh
@@ -1,12 +1,11 @@
-s3_bucket=$S3_BUCKET
-for i in $(find . -name '*.yml'); do
+for i in $(find ./src -not -path '*/.aws-sam/*' -not -path '*/node_modules/*' -name '*.yml'); do
     filename=$(basename $i)
     directory=$(dirname $i)
     yml_to_yaml=${filename/yml/yaml}
     path_for_compiled_templates='templates/'$yml_to_yaml
     echo $path_for_compiled_templates
     sam build -s $directory -t $i
-    sam package --template-file $i --s3-bucket $s3_bucket --output-template-file $path_for_compiled_templates
+    sam package --template-file $i --s3-bucket $S3_BUCKET --output-template-file $path_for_compiled_templates
 done
-aws s3 cp templates s3://$s3_bucket --recursive
+aws s3 cp templates s3://$S3_BUCKET --recursive
 rm -rf .aws-sam/
