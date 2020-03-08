b179e0691eab4399bfb21b0de91263774ff75c20
Nathaniel Gumbs <ngumbs37@gmail.com>
2020-03-07 23:40:17 -0600

    change from react to vue

---
+++ source/web/src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='card-list shaded'>
+    <inventory-card/>
+
+    <inventory-card class="mt-1"/>
+
+    <img class="other-image mt-1" src="Screen7.55.04.png" />
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.card-list {
+  padding: 1rem;
+  width: 100%;
+}
+.other-image {
+  max-height: 100%;
+  max-width: 100%;
+  background-repeat: no-repeat;
+  margin-top: 1rem;
+}
+.mt-1{
+  margin-top: 1rem;
+}
+</style>
